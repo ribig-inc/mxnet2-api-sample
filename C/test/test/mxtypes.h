7d45ea4ed64483d5ca31612f1b3410b5de6caaf9
ribig-inc <morikawa@ribig.co.jp>
2024-11-02 16:35:12 +0900

    Update mxtypes.h

--- C/test/test/mxtypes.h
+++ C/test/test/mxtypes.h
@@ -8,29 +8,34 @@
 #ifndef _MATRIXAPI_DATA_TYPES
 #define _MATRIXAPI_DATA_TYPES
 
-#include <limits.h>
 
 #ifdef __cplusplus
-extern "C" {
-#endif
+	
+	#include <cstdint>
 
-#if ULONG_MAX == 0xffffffff
-  #define  _mxINT32  long
-  #define  _mxUINT32 unsigned long
-  #define  _mxINT16  short
-  #define  _mxUINT16 unsigned short
-#elif UINT_MAX == 0xffffffff
-  #define  _mxINT32  int
-  #define  _mxUINT32 unsigned int
-  #define  _mxINT16  short
-  #define  _mxUINT16 unsigned short 
+	using _mxINT32=std::int32_t;
+	using _mxUINT32=std::uint32_t;
+	using _mxINT16=std::int16_t;
+	using _mxUINT16=std::uint16_t;
+
 #else
-  #error can not define Matrix-API data types
-#endif
 
+	#include <limits.h>
 
-#ifdef __cplusplus
-}
+	#if ULONG_MAX == 0xffffffff
+	  #define  _mxINT32  long
+	  #define  _mxUINT32 unsigned long
+	  #define  _mxINT16  short
+	  #define  _mxUINT16 unsigned short
+	#elif UINT_MAX == 0xffffffff
+	  #define  _mxINT32  int
+	  #define  _mxUINT32 unsigned int
+	  #define  _mxINT16  short
+	  #define  _mxUINT16 unsigned short 
+	#else
+	  #error can not define Matrix-API data types
+	#endif
+
 #endif
 
 #endif  // _MATRIXAPI_DATA_TYPES
